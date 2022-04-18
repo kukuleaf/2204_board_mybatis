b2d804e731be277aaff66414ba770471e4ecf431
kukuleaf <dbsdugns16@gmail.com>
2022-04-19 06:01:31 +0900

    board_mybatis

---
+++ Desktop/coding/jsp/source/jsp07_board_mybatis/src/main/java/service/ReplyService.java
@@ -0,0 +1,42 @@
+package service;
+
+import java.util.List;
+
+import dao.ReplyDAO;
+import dto.Reply;
+
+public class ReplyService {
+	private ReplyDAO replyDAO = new ReplyDAO();
+	
+	public void insert(Reply reply) {
+		//댓글의순서(restep)
+		reply.setRestep(reply.getRestep()+1); 
+		//댓글의레벨(relevel)
+		reply.setRelevel(reply.getRelevel()+1);
+		
+		//기존댓글의 순서를 수정 
+		replyDAO.update_restep(reply);
+		
+		int cnt = replyDAO.insert(reply);
+		System.out.println(cnt+"건 추가");
+	}
+
+	public void update(Reply reply) {
+		int cnt = replyDAO.update(reply);
+		System.out.println(cnt+"건 댓글 수정");
+	}
+	
+	public List<Reply> selectList(int bnum) {
+		return replyDAO.selectList(bnum);
+	}
+	
+	public void delete(int rnum) {
+		replyDAO.delete(rnum);
+	}
+	
+	public Reply selectOne(int rnum) {
+		return replyDAO.selectOne(rnum);
+	}
+	
+	
+}
